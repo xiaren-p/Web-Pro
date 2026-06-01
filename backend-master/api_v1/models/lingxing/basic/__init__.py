8b328617f131728eeecc62e8da36bdf1f9642006
company <2724644958@qq.com>
2026-06-01 18:54:27 +0800

    优化

--- backend-master/api_v1/models/lingxing/basic/__init__.py
+++ backend-master/api_v1/models/lingxing/basic/__init__.py
@@ -1,3 +1,11 @@
 from api_v1.models.lingxing.basic.lx_shops import HasAdsSetting, LxShops, ShopStatus
+from api_v1.models.lingxing.basic.lx_user import IsMaster, LxUser, UserStatus
 
-__all__ = ['LxShops', 'ShopStatus', 'HasAdsSetting']
+__all__ = [
+    "LxShops",
+    "ShopStatus",
+    "HasAdsSetting",
+    "LxUser",
+    "UserStatus",
+    "IsMaster",
+]
