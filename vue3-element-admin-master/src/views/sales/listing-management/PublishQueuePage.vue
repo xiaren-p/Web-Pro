9365bf5f6825308780fb7e5535898b4b641df202
PCH <2724644958@qq.com>
2026-07-02 10:09:10 +0800

    fix: 恢复 4 个 placeholder 页（被 index.vue 引用，删后构建失败）

---
+++ vue3-element-admin-master/src/views/sales/listing-management/PublishQueuePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="placeholder-page">
+    <el-result icon="info" title="刊登队列" sub-title="功能开发中，敬请期待" />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.placeholder-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 400px;
+}
+</style>
