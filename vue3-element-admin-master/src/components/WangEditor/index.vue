f2810e765f791032de26b02f14815045f551ff7f
PCH <2724644958@qq.com>
2026-07-05 19:13:54 +0800

    fix(WangEditor): MENU_CONF类型断言回退为as any(wangeditor v5类型不兼容)

--- vue3-element-admin-master/src/components/WangEditor/index.vue
+++ vue3-element-admin-master/src/components/WangEditor/index.vue
@@ -56,7 +56,8 @@ const editorConfig = ref<Partial<IEditorConfig>>({
           ElMessage.error(err instanceof Error ? err.message : "图片上传失败");
         }
       },
-    } as IEditorConfig["MENU_CONF"],
+    // wangeditor v5 类型系统复杂，MENU_CONF.uploadImage 类型不兼容 Partial<IEditorConfig>，需 any 绕过
+    } as any,
   },
 });
 
